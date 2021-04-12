4997067f4447dcef75d65335ef37eea68fb4be1c
Vadim <vertersssr@mail.ru>
2021-04-12 16:33:28 +0800

    slider

--- puls/src/js/script.js
+++ puls/src/js/script.js
@@ -1,8 +1,37 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1500,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+//слик сллайдер
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1500,
+//         adaptiveHeight: true,
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="../img/arrowLeft.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="../img/arrowRight.png"></button>',
+//         responsive:[
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                 dots: true,
+//                 arrows: false
+//                 }
+//               }
+//         ]
+//     });
+//   });
+//слик сллайдер
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    speed: 1200,
+    controls: false,
+    navPosition: 'bottom'
   });
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});  
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});  
