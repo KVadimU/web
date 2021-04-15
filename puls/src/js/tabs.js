f70aeef7ec0612f1b3eb084cbcdfd079b66b0300
Vadim <vertersssr@mail.ru>
2021-04-15 13:55:38 +0800

    Дописал скрипт с табами

--- puls/src/js/tabs.js
+++ puls/src/js/tabs.js
@@ -1,15 +1,23 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', function(){
     let tabs = document.querySelector('.catalog__tabs'),
-        tab = document.querySelectorAll('.catalog__tab');
-
+        tab = document.querySelectorAll('.catalog__tab'),
+        catalog = document.querySelectorAll('.catalog__content');
+       
         tabs.addEventListener('click', function(event){
-            let li = event.target.closest('li');//closset()-возвращает ближайшего предка
+            let li = event.target.closest('li'),//closset()-возвращает ближайшего предка
+                tabIndex;
             if(li && !li.classList.contains('catalog__tab_active')){
-            for(let i=0; i<tab.length; i++){
-                tab[i].classList.remove('catalog__tab_active');
-                if(li == tab[i]){
+            for(tabIndex=0; tabIndex<tab.length; tabIndex++){
+                tab[tabIndex].classList.remove('catalog__tab_active');
+                if(li == tab[tabIndex]){
                     li.classList.add('catalog__tab_active');
+                    catalog.forEach(function(item, i, catalog){
+                        item.classList.remove('catalog__content_active');
+                       if(tabIndex==i){
+                           item.classList.add('catalog__content_active');
+                       }
+                    });
                 }
              }
             }
