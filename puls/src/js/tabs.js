a93c0a4bd5a1b923213f3fe2210f885b52371fe0
Vadim <vertersssr@mail.ru>
2021-05-01 14:14:58 +0800

    маска ввода

--- puls/src/js/tabs.js
+++ puls/src/js/tabs.js
@@ -98,13 +98,56 @@ window.addEventListener('DOMContentLoaded', function(){
                 
             });
         });
+        ////////////////////////////////////////////////////////////////////////
+        //mask input
+        ////////////////////////////////////////////////////////////////////////
+        let input = document.querySelectorAll('[name="phone"]');
+            //input.addEventListener("input", mask, false);
+            input.forEach(function(item, i, input){
+                item.addEventListener('input', mask, false);
+           
+        function setCursorPosition(pos, item) {
+                item.focus();
+                if (item.setSelectionRange) item.setSelectionRange(pos, pos);
+                else if (item.createTextRange) {
+            let range = item.createTextRange();
+            range.collapse(true);
+            range.moveEnd("character", pos);
+            range.moveStart("character", pos);
+            range.select()
+           }
+            }
+        
+            function mask(event){
+                let matrix = this.defaultValue,
+                    i = 0,
+                    def = matrix.replace(/\D/g, ""),
+                    val = this.value.replace(/\D/g, "");
+                    def.length >= val.length && (val = def);
+                matrix = matrix.replace(/[_\d]/g, function(a) {
+                    return val.charAt(i++) || "_"
+                });
+                this.value = matrix;
+                i = matrix.lastIndexOf(val.substr(-1));
+                i < matrix.length && matrix != this.defaultValue ? i++ : i = matrix.indexOf("_");
+                setCursorPosition(i, this)
+        
+            }
+        });
+        ////////////////////////////////////////////////////////////////////////
         //validationForm
+        ///////////////////////////////////////////////////////////////////////
+
         let formsConsultation = document.querySelectorAll('.feed-form')[1],
+            formsConsultation2 = document.querySelectorAll('.feed-form')[0],
             //regPhone = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
             regPhone2 = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){6,14}(\s*)?$/,
-            regName = /^[a-zA-Zа-яА-Я '.-]*$/;
-
+            regName = /^[a-zA-Zа-яА-Я '.-]*$/,
+            error = 0;
+           
+        
             formsConsultation.addEventListener('submit', formSend);
+           
 
             async function formSend(e){
                 e.preventDefault();
@@ -133,8 +176,8 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         
             function formValidate(formsConsultation){
-                let error = 0,
-                    formInp = formsConsultation.querySelectorAll('input');
+               
+                   let formInp = formsConsultation.querySelectorAll('input');
                  for (let i = 0; i < formInp.length; i++) {
                      const input = formInp[i];
                      console.log(input);
@@ -172,6 +215,64 @@ window.addEventListener('DOMContentLoaded', function(){
                 return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
 
             }
+        
+            ///////////////////////////////////////////////////
+            //форма консультации 2
+            /////////////////////////////////////////////////////
+            async function formSend2(e){
+                e.preventDefault();
+                let error = formValidate2(formsConsultation2),
+                    formData = new FormData(formsConsultation2);
+                if(error === 0){
+                   // formsConsultation.classList.add('feed-form_sending');
+                    let response = await fetch('sendmail.php',{
+                        method: 'POST',
+                        body: formData
+                    });
+                    if(response.ok){
+                        let result = await response.json();
+                        alert(result.message);
+                        formsConsultation2.reset();
+                       // formsConsultation.classList.remove('feed-form_sending');
+                    }else{
+                        alert("Ошибка!");
+                       //formsConsultation.classList.remove('feed-form_sending');
+                    }
+                }else{
+                   alert("Заполните все поля!");
+                }
+
+            }
+        
+            formsConsultation2.addEventListener('submit', formSend2);
+            function formValidate2(formsConsultation2){
+                let formInp = formsConsultation2.querySelectorAll('input');
+                 for (let i = 0; i < formInp.length; i++) {
+                     const input = formInp[i];
+                     console.log(input);
+                     removeError(input);
+                     if(i == 2){   //проверяем email
+                         console.log(input.value);
+                         if(testEmail(input)){
+                             addError(input);
+                             error++;
+                         }
+                     }else if(input.value === ''){
+                         addError(input);
+                         error++;
+
+                     }else if(i == 1 && !regPhone2.test(input.value)){ //проверяем телефон
+                        addError(input);
+                        error++;
+                     }else if(i == 0 && !regName.test(input.value)){
+                         addError(input);
+                         error++;
+                     }
+                     
+                 }
+                 return error;
+            }
+
       
 
 });
