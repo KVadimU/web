1893cc6e04e5e7d0d70f0de5df752a5ccc2952e2
Vadim <vertersssr@mail.ru>
2021-06-07 17:44:42 +0800

    финиш

--- portfolio/dist/js/script.js
+++ portfolio/dist/js/script.js
@@ -19,37 +19,37 @@ window.addEventListener('DOMContentLoaded', function(){
     ////sidepanel////
     ///////////////
     /////////////
-    document.addEventListener('scroll', ()=>{
-        let sidePanelText = document.querySelector('.sidepanel__text'),
-            sidePanelDivider = document.querySelector('.sidepanel__divider'),
-            linkVk = document.querySelectorAll('.sidepanel__link')[0],
-            linkGitHub = document.querySelectorAll('.sidepanel__link')[1];
-        //console.log(window.pageYOffset);
-        if(window.pageYOffset > 300){
-            sidePanelText.style.color = '#000000';      
-        };
-        if(window.pageYOffset > 440){
-            sidePanelDivider.style.background = '#000000';
-        };
-        if(window.pageYOffset > 520){
-            linkGitHub.firstChild.setAttribute("src", "img/icons/github_green.svg");
-        };
-        if(window.pageYOffset > 600){
-            linkVk.firstChild.setAttribute("src", "img/icons/vk_red2.png");
-        };
-        if(window.pageYOffset < 300){
-            sidePanelText.style.color = '#FFFFFF';
-        }
-        if(window.pageYOffset < 440){
-            sidePanelDivider.style.background = '#FFFFFF';
-        };
-        if(window.pageYOffset < 520){
-            linkGitHub.firstChild.setAttribute("src", "img/icons/github.svg");
-        };
-        if(window.pageYOffset < 600){
-            linkVk.firstChild.setAttribute("src", "img/icons/vk.png");
-        };
-    });
+    // document.addEventListener('scroll', ()=>{
+    //     let sidePanelText = document.querySelector('.sidepanel__text'),
+    //         sidePanelDivider = document.querySelector('.sidepanel__divider'),
+    //         linkVk = document.querySelectorAll('.sidepanel__link')[0],
+    //         linkGitHub = document.querySelectorAll('.sidepanel__link')[1];
+    //     //console.log(window.pageYOffset);
+    //     if(window.pageYOffset > 300){
+    //         sidePanelText.style.color = '#000000';      
+    //     };
+    //     if(window.pageYOffset > 440){
+    //         sidePanelDivider.style.background = '#000000';
+    //     };
+    //     if(window.pageYOffset > 520){
+    //         linkGitHub.firstChild.setAttribute("src", "img/icons/github_green.svg");
+    //     };
+    //     if(window.pageYOffset > 600){
+    //         linkVk.firstChild.setAttribute("src", "img/icons/vk_red2.png");
+    //     };
+    //     if(window.pageYOffset < 300){
+    //         sidePanelText.style.color = '#FFFFFF';
+    //     }
+    //     if(window.pageYOffset < 440){
+    //         sidePanelDivider.style.background = '#FFFFFF';
+    //     };
+    //     if(window.pageYOffset < 520){
+    //         linkGitHub.firstChild.setAttribute("src", "img/icons/github.svg");
+    //     };
+    //     if(window.pageYOffset < 600){
+    //         linkVk.firstChild.setAttribute("src", "img/icons/vk.png");
+    //     };
+    // });
         ////////////////
       //////////////////
     ////progress bar////
